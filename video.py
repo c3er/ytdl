cfc988d25219863aa3edcf20cb00f64df5f37b5f
Christian Dreier <flipper.x3r@googlemail.com>
2020-06-21 18:52:35 +0200

    First approach to display download progress

--- video.py
+++ video.py
@@ -1,3 +1,4 @@
+import enum
 import json
 import os
 import subprocess
@@ -7,7 +8,37 @@ import pytube
 import pytube.helpers
 
 
+_on_download_progress = None
+_on_download_completed = None
+
+
+class FileType(enum.Enum):
+    VIDEO = enum.auto()
+    AUDIO = enum.auto()
+    UNKNOWN = enum.auto()
+
+    @property
+    def label(self):
+        return {
+            self.VIDEO: "video",
+            self.AUDIO: "audio",
+            self.UNKNOWN: "unknown",
+        }[self]
+
+
+class FileInfo:
+    def __init__(self):
+        self.filetype = FileType.UNKNOWN
+        self.path = ""
+
+    def set(self, filetype, path):
+        self.filetype = filetype
+        self.path = path
+
+
 class Video:
+    _fileinfo = FileInfo()
+
     def __init__(self, title, link):
         self.title = title
         self.link = link
@@ -17,7 +48,8 @@ class Video:
         return pytube.helpers.safe_filename(self.title)
 
     @classmethod
-    def collect(cls, path):
+    def collect(cls, path, progress_handler, completed_handler):
+        cls._register_handlers(progress_handler, completed_handler)
         with open(path, encoding="utf8") as f:
             return list(reversed([
                 cls(v["title"], v["link"])
@@ -25,15 +57,25 @@ class Video:
             ]))
 
     def download(self, outdir, filename):
-        streams = pytube.YouTube(self.link).streams
+        streams = pytube.YouTube(
+            self.link,
+            on_progress_callback=_on_download_progress,
+            on_complete_callback=_on_download_completed
+        ).streams
+
         video = (streams
             .filter(mime_type="video/mp4")
             .order_by('resolution')[-1])
+
         if video.includes_audio_track:
+            self._fileinfo.set(FileType.VIDEO, os.path.join(outdir, f"{filename}"))
             video.download(output_path=outdir, filename=filename)
         else:
             with tempfile.TemporaryDirectory() as tmpdirname:
+                self._fileinfo.set(FileType.VIDEO, os.path.join(tmpdirname, "video.mp4"))
                 video.download(output_path=tmpdirname, filename="video")
+
+                self._fileinfo.set(FileType.VIDEO, os.path.join(tmpdirname, "audio.mp4"))
                 (streams
                     .get_audio_only()
                     .download(output_path=tmpdirname, filename="audio"))
@@ -50,3 +92,21 @@ class Video:
                     "-shortest",
                     os.path.join(outdir, f"{filename}.mp4")
                 ])
+
+    @classmethod
+    def _register_handlers(cls, progress_handler, completed_handler):
+        global _on_download_progress
+        global _on_download_completed
+
+        completed = 0
+
+        def on_progress_callback(stream, chunk, bytes_remaining):
+            nonlocal completed
+            completed += len(chunk)
+            progress_handler(cls._fileinfo, completed, bytes_remaining)
+
+        def on_complete_callback(stream, file_path):
+            completed_handler(cls._fileinfo)
+
+        _on_download_progress = on_progress_callback
+        _on_download_completed = on_complete_callback
--- ytdl.py
+++ ytdl.py
@@ -1,3 +1,4 @@
+import math
 import os
 import sys
 import traceback
@@ -13,27 +14,45 @@ def log(*msg, sep=" ", end="\n", file=sys.stdout):
     file.flush()
 
 
-def main():
-    outdir = sys.argv[1]
-    videos = video.Video.collect(os.path.join(starterdir, "videos.json"))
-    video_count = len(videos)
-    digit_count = len(str(video_count))
-    log(f"Downloading {video_count} videos...")
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    for i, v in enumerate(videos):
-        filename = f"{str(i + 1).rjust(digit_count, '0')} {v.filename}"
-        if os.path.exists(os.path.join(outdir, f"{filename}.mp4")):
-            log(f'Video "{v.title}" does already exist.')
-            continue
-        log(f'Download video "{v.title}"...', end="\t")
-        try:
-            v.download(outdir, filename)
-            log("Done")
-        except Exception:
-            log("Error", file=sys.stderr)
-            log(traceback.format_exc(), file=sys.stderr)
+def error(*msg, shall_exit=True):
+    log(*msg, file=sys.stderr)
+    if shall_exit:
+        sys.exit(1)
 
+
+def download_progress_handler(fileinfo, completed, remaining):
+    log(f'{fileinfo.filetype.label.title()}: "{fileinfo.path}"\t{(completed / (remaining + completed)) * 100}%')
+
+
+def download_completed_handler(fileinfo):
+    log(f'"{fileinfo.path}" downloaded')
+
+
+def main():
+    try:
+        outdir = sys.argv[1]
+        videos = video.Video.collect(
+            os.path.join(starterdir, "videos.json"),
+            download_progress_handler,
+            download_completed_handler)
+        video_count = len(videos)
+        digit_count = int(math.log10(video_count)) + 1
+        log(f"Downloading {video_count} videos...")
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        for i, v in enumerate(videos):
+            filename = f"{str(i + 1).rjust(digit_count, '0')} {v.filename}"
+            if os.path.exists(os.path.join(outdir, f"{filename}.mp4")):
+                log(f'Video "{v.title}" does already exist.')
+                continue
+            log(f'Download video "{v.title}"...')
+            try:
+                v.download(outdir, filename)
+            except Exception:
+                error("Error", shall_exit=False)
+                error(traceback.format_exc())
+    except KeyboardInterrupt:
+        error("Aborted")
 
 
 if __name__ == "__main__":
