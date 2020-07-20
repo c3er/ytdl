1327489077e876348944c06e2b1d8f714945af90
Christian Dreier <flipper.x3r@googlemail.com>
2020-07-20 03:24:58 +0200

    Minor improvement at download display

--- ytdl.py
+++ ytdl.py
@@ -57,7 +57,7 @@ def download_progress_handler(fileinfo, completed, remaining):
 
 
 def download_completed_handler(fileinfo):
-    progress.complete(f'"{fileinfo.path}" downloaded')
+    progress.complete(f'"{fileinfo.path}" downloaded\n')
     _bandwidth.reset()
 
 
